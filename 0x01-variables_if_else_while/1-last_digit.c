41b958c127e45fe2d335774497a4031cf2182335
Micheal Oriyomi <michealoriyomi01@gmail.com>
2023-02-19 18:16:26 +0100

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,25 +14,27 @@
 int main(void)
 {
 	int n;
+	int mod;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	mod = n % 10;
 
 	if (n >= 0)
 	{
-		if ((n % 10) > 5)
-			printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10));
-		else if ((n % 10) == 0)
-			printf("Last digit of %d is %d and is 0\n", n, (n % 10));
-		else if (((n % 10) < 6) && ((n % 10) != 0))
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
+		if ((mod) > 5)
+			printf("Last digit of %d is %d and is greater than 5\n", n, (mod));
+		else if ((mod) == 0)
+			printf("Last digit of %d is %d and is 0\n", n, (mod));
+		else if (((mod) < 6) && ((mod) != 0))
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (mod));
 	}
 	else if (n < 0)
 	{
-		if (((n % 10) * -1) == 0)
-			printf("Last digit of  %d is %d and is 0\n", n, ((n % 10) * -1));
-		else if (((n % 10) < 6) && (((n % 10) * -1) != 0))
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
+		if (((mod) * -1) == 0)
+			printf("Last digit of  %d is %d and is 0\n", n, ((mod) * -1));
+		else if (((mod) < 6) && (((mod) * -1) != 0))
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (mod));
 	}
 	return (0);
 }
